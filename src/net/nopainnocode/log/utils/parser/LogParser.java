95192834d5fbc74c99703fd52b6bfb4509125df7
01B <no.pain.no.code@gmail.com>
2015-12-01 22:41:16 +0900

    refactoring parser

--- src/net/nopainnocode/log/utils/parser/LogParser.java
+++ src/net/nopainnocode/log/utils/parser/LogParser.java
@@ -4,36 +4,42 @@ import net.nopainnocode.log.domain.Log;
 import net.nopainnocode.log.domain.UrlInfo;
 import net.nopainnocode.log.domain.type.Status;
 import net.nopainnocode.log.domain.type.WebBrower;
-import net.nopainnocode.log.utils.formatter.Formatter;
 
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.util.*;
 
 /**
  * Created by no_pain_no_code on 2015. 11. 29..
  * 불러온 input 로그 정보를 분석하기 쉽게 데이터를 파싱한다.
  */
-public class LogParser implements Parser {
+public class LogParser<K, V> implements Parser<K, V> {
 
     /**
      * 로그 정보를 쪼개어 저장한다.
      * @param str
-     * @param formatter
      */
     @Override
-    public void parse(String str, Formatter formatter) {
+    public Map<K, List<V>> parse(String str) {
 
+        Map<K, List<V>> logMap = new HashMap<>();
         String[] line = str.replaceAll("\\[", "").split("\n");
         int len = line.length;
 
-        for (int i = 0; i < len; i++)
-            formatter.addLog(generateLog(line[i].split("\\]")));
+        for (int i = 0; i < len; i++) {
+            Log log = generateLog(line[i].split("\\]"));
+            Status key = log.getStatus();
+            List<V> logs = logMap.getOrDefault(key, new ArrayList<>());
+            logs.add((V)log);
+            logMap.put((K)key, logs);
+        }
+        return logMap;
     }
 
     /**
      * 로그 정보를 만듧니다.
+     *
      * @param logSplit
      * @return
      */
--- src/net/nopainnocode/log/utils/parser/Parser.java
+++ src/net/nopainnocode/log/utils/parser/Parser.java
@@ -1,11 +1,12 @@
 package net.nopainnocode.log.utils.parser;
 
-import net.nopainnocode.log.utils.formatter.Formatter;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by no_pain_no_code on 2015. 11. 29..
  * 로그 정보를 파싱한다.
  */
-public interface Parser {
-    void parse(String str, Formatter formatter);
+public interface Parser<K, V> {
+    Map<K, List<V>> parse(String str);
 }
